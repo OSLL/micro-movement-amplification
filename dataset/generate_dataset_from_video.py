21382c411af0e6ce20e2dddd0e6b56e069565367
light5551 <sergey.glazunov.99@mail.ru>
2023-01-31 01:25:30 +0300

    fix error save elements

--- dataset/generate_dataset_from_video.py
+++ dataset/generate_dataset_from_video.py
@@ -1,32 +1,50 @@
+from collections import deque
+from time import sleep
 import numpy as np
 import argparse
+import shutil
 import cv2
+import sys
 import os
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", default=None, help="path to video")
-    parser.add_argument("--length", default=3, help="Length of frame sequence")
+    parser.add_argument("--maxlen", default=3, help="Length of frame sequence")
     parser.add_argument("--dir", default="data", help="destination for dataset")
-    return parser
+    return parser.parse_args()
 
 
 def generate(parser):
     if not parser.file:
         return
+    deque_frames = deque(maxlen=int(parser.maxlen))
     cap = cv2.VideoCapture(parser.file)
 
     if not cap.isOpened():
         print("Error opening video stream or file")
 
+    # remove and create directory of dataset
+    if os.path.exists(os.path.join(os.getcwd(), parser.dir)):
+        shutil.rmtree(parser.dir)
+    os.mkdir(parser.dir)
+
+    idx = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
-            cv2.imshow('Frame', frame)
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+            deque_frames.append(frame)
+            if len(deque_frames) == deque_frames.maxlen:
+                idx += 1
+                path_element = f"{os.getcwd()}/{parser.dir}/imgs{idx}"
+                os.mkdir(path_element)
+                # frame{chr(65+)}
+                for index, frame in enumerate(deque_frames):
+                    filename = f"{path_element}/frame{chr(65 + index)}.jpg"
+                    status = cv2.imwrite(filename, frame)
+                    print(f"Image {filename} to file-system : {status}")
+                # save_element(idx, deque_frames.copy())
         else:
             break
     cap.release()
